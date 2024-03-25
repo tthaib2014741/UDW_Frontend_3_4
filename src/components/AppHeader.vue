6afc6606cc44e12cfaf74647bb747f5060e05dde
thanhhaii122 <haib2014741@student.ctu.edu.vn>
2024-03-25 20:27:01 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-Lx4Ln2Z0SgsNExaUeOAZlZTfS/+NDPa0T1bR1XF6XySiPf1EF3/aPuVMaNoXk1naI+8sR0ubpV0mZ8l93BlpHg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    Danh bạ
+                    <i class="fas fa-address-book"></i>
+                </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+ 
+import router from "./router";
+
+
+createApp(App).use(router).mount('#app')
