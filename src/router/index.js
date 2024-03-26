835faaeb3c41a321209689872f62bd1a248aef07
thanhhaii122 <haib2014741@student.ctu.edu.vn>
2024-03-26 13:16:21 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,22 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
+
     },
+    
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+     },
+     {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true 
+        },
+        
+
     ];
         const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -13,13 +13,13 @@
             <p v-else>Không có liên hệ nào.</p>
             <div class="mt-3 row justify-content-around align-items-center">
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
-                   <i class="fas fa-redo"></i> Làm mới
+                    <i class="fas fa-redo"></i> Làm mới
                 </button>
                 <button class="btn btn-sm btn-success" @click="goToAddContact()">
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts()">
-                   <i class="fas fa-trash"></i> Xóa tất cả
+                    <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
             </div>
         </div>
@@ -30,6 +30,14 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                    }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
+
             </div>
         </div>
     </div>
